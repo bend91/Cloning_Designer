fc7dca07d6eacaf0df4f0b14988dabe9112ec265
bend91 <benowendraper@gmail.com>
2024-10-23 15:25:18 +0100

    2024-10-23 Improved command line use

--- main.py
+++ main.py
@@ -16,24 +16,36 @@ def main():
 
 
     primer_list = primer_designer(fasta_data)
-    opt_primers = identify_optimal_annealing_temp(primer_list)
+    if input("Define a custom annealing temperature? If no then annealing temperature will be based on the least variation between primer sequences\n (Y/N): ") in "Yesyes":
+        opt_tm = float("Enter annealing temperature to aim for: ")
+    else:
+        opt_tm = None
+    opt_primers = identify_optimal_annealing_temp(primer_list, opt_tm)
     print("Enter the strategy you would like to use for cloning: \n")
     [print(f"[{i}] {x}\n") for i, x in enumerate(strategy_list)]
-    chosen_strategy = strategy_list[int(input(""))]
-
+    chosen_strategy = strategy_list[int(input("> "))]
     designed_primers = strategy[chosen_strategy](fasta_data, opt_primers)
-    print(designed_primers)
-    for i, data in enumerate(fasta_data):
-        print(data.name)
-        if i == len(fasta_data):
-            break
-        if i > 0:
-            print("Forward: ", designed_primers["Forward"][i-1])
-        if i < len(fasta_data) - 1:
-            print("Reverse: ", designed_primers["Reverse"][i])
-    # with open("cloning_plan.txt", "w") as f:
-        # for i in fasta_data:
-        # f.writelines(lines: Iterable[str])
+
+    if input("Enter filepath to write plan to (otherwise will just be saved in this directory as cloning_plan.txt) (Y/N)? ") in "Yesyes":
+        save_file_dir = input("Enter path to save the file to: ")
+        save_file_name = input("Enter the name you would like for the file: ")
+        if save_file_dir[0] != "/":
+            save_file_dir = save_file_dir + "/"
+        save_file = save_file_dir + save_file_name
+    else:
+        save_file = "cloning_plan.txt"
+
+    with open(save_file, "w") as f:
+        for i, data in enumerate(fasta_data):
+            f.write(f"{data.name}\n")
+            if i == len(fasta_data):
+                break
+            if i > 0:
+                f.write(f" - Forward: {designed_primers['Forward'][i-1].sequence}\n")
+            if i < len(fasta_data) - 1:
+                f.write(f" - Reverse: {designed_primers['Reverse'][i].sequence}\n")
+            f.write("\n")
+        f.write(f"Optimal PCR annealing temperature is {round(sum([x.tm for x in opt_primers['Forward']] + [x.tm for x in opt_primers['Reverse']]) / len(opt_primers['Forward'] + opt_primers['Reverse']), 2) + 5}\n")
     ...
 
 
