ba4cf9f4a55f286c2f7c5486ec43c2179cf14e9f
bend91 <benowendraper@gmail.com>
2024-10-23 10:55:47 +0100

    2024-10-23 Internal HiFi primer design

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
 from functions import *
 
 
+strategy_list = [
+    "HiFi"
+]
+
+strategy = {
+    "HiFi": design_hifi_primers,
+}
+
+
 def main():
     fasta_filepath = input("Enter path to fasta file containing parts to clone together, in order: ")
     fasta_data = fasta_parser(fasta_filepath)
+
+
     primer_list = primer_designer(fasta_data)
     opt_primers = identify_optimal_annealing_temp(primer_list)
-    hifi_primers = design_hifi_primers(fasta_data, opt_primers)
-    print(hifi_primers)
+    print("Enter the strategy you would like to use for cloning: \n")
+    [print(f"[{i}] {x}\n") for i, x in enumerate(strategy_list)]
+    chosen_strategy = strategy_list[int(input(""))]
+
+    designed_primers = strategy[chosen_strategy](fasta_data, opt_primers)
+    print(designed_primers)
     for i, data in enumerate(fasta_data):
         print(data.name)
         if i == len(fasta_data):
             break
         if i > 0:
-            print("Forward: ", hifi_primers["Forward"][i-1])
+            print("Forward: ", designed_primers["Forward"][i-1])
         if i < len(fasta_data) - 1:
-            print("Reverse: ", hifi_primers["Reverse"][i])
+            print("Reverse: ", designed_primers["Reverse"][i])
     # with open("cloning_plan.txt", "w") as f:
         # for i in fasta_data:
         # f.writelines(lines: Iterable[str])
